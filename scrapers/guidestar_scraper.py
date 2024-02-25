f474c81de31d47e9fc5bd252b48fe48d816eb785
Hariaksha <hariakshagunda@gmail.com>
2024-02-25 13:49:58 -0600

    fixed internet connection issue
    program now pauses whenever internet disconnects, instead of just breaking the program

--- scrapers/guidestar_scraper.py
+++ scrapers/guidestar_scraper.py
@@ -4,18 +4,16 @@ import openpyxl
 import csv
 import time
 
-def check_internet(url='https://www.google.com', timeout=5):
-    # put the below three lines in the for loop in the main function to implement 
-    # while check_internet() is False:
-    #     print("Connection lost")
-    #     time.sleep(1)
-    try:
-        # Try to request Google. If successful, internet is working.
-        _ = requests.get(url, timeout=timeout)
-        return True
-    except requests.ConnectionError:
-        # Internet is not working.
-        return False 
+def get_request(link):
+    while True:  
+        try:
+            x = requests.get(link)
+            ans = BeautifulSoup(x.content, 'html.parser')
+            break
+        except:
+            print('Connection lost')
+            time.sleep(10)
+    return ans
 
 def numToEIN(num):
     ans = format(num, '09d')
@@ -24,20 +22,16 @@ def numToEIN(num):
 
 def main():
     # MUST CHANGE STATE ABBREVIATION FOUR TIMES IN THIS FUNCTION
-    filename = open('exempt_organizations/eo_me.csv') # CHANGE
+    filename = open('exempt_organizations/eo_nh.csv') # CHANGE
     file = csv.DictReader(filename)
-    workbook = openpyxl.load_workbook('data/ME_data.xlsx') # CHANGE
+    workbook = openpyxl.load_workbook('data/NH_data.xlsx') # CHANGE
     ws = workbook.active
     ws2 = workbook['Skipped']
     for col in file:
-        while check_internet() is False:
-            print("Connection lost")
-            time.sleep(5)   
         time.sleep(0.90) # 0.84 is too fast
         ein = numToEIN(int(col['EIN']))
         link = f"https://www.guidestar.org/profile/{ein}"
-        r = requests.get(link) 
-        soup = BeautifulSoup(r.content, 'html.parser')
+        soup = get_request(link)
         print(soup.title.text)
         if soup.title.text == "": 
             # if GuideStar does not have a page for this org, add it to the skipped list
@@ -52,8 +46,8 @@ def main():
         name = soup.find('h1', class_='profile-org-name').text.strip()
         url = '' if soup.find('a', class_='hide-print-url') == None else soup.find('a', class_='hide-print-url').text
         # CHANGE
-        ws.append([ein, name, col['STREET'], col['CITY'], 'ME', col['ZIP'], link, url, col['NTEE_CD'], col['DEDUCTIBILITY'], col['ASSET_CD'], col['ASSET_AMT'], col['INCOME_CD'], col['INCOME_AMT'], col['REVENUE_AMT'], mission])
-    workbook.save('data/ME_data.xlsx') # CHANGE
+        ws.append([ein, name, col['STREET'], col['CITY'], 'NH', col['ZIP'], link, url, col['NTEE_CD'], col['DEDUCTIBILITY'], col['ASSET_CD'], col['ASSET_AMT'], col['INCOME_CD'], col['INCOME_AMT'], col['REVENUE_AMT'], mission])
+    workbook.save('data/NH_data.xlsx') # CHANGE
     
 if __name__=="__main__":
     main()
