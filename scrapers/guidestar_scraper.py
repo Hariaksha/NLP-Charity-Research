04d320b752ac689f702f53625705727354710874
Hariaksha <hariakshagunda@gmail.com>
2024-03-11 20:13:46 -0500

    Update guidestar_scraper.py
    print EIN
    check for error 502: bad gateway for each site

--- scrapers/guidestar_scraper.py
+++ scrapers/guidestar_scraper.py
@@ -21,7 +21,7 @@ def numToEIN(num):
     return ans
 
 def main():
-    state = 'TN' # CHANGE
+    state = 'WA' # CHANGE
     filename = open(f'exempt_organizations/eo_{state.lower()}.csv') 
     file = csv.DictReader(filename)
     workbook = openpyxl.load_workbook(f'data/{state}_data.xlsx')
@@ -32,7 +32,9 @@ def main():
         ein = numToEIN(int(col['EIN']))
         link = f"https://www.guidestar.org/profile/{ein}"
         soup = get_request(link)
-        print(soup.title.text)
+        while soup.title.text == "www.guidestar.org | 502: Bad gateway":
+            soup = get_request(link)
+        print(ein, soup.title.text)
         if soup.title.text == "": 
             # if GuideStar does not have a page for this org, add it to the skipped list
             ws2.append([ein, col['NAME'], link])
