e4232adb77f74ae77d1d76327615372c9560b07d
Hariaksha <hariakshagunda@gmail.com>
2024-03-14 13:34:13 -0500

    Update complexity.r

--- nlp/complexity.r
+++ nlp/complexity.r
@@ -1,13 +1,20 @@
+# https://towardsdatascience.com/linguistic-complexity-measures-for-text-nlp-e4bf664bd660
 library(quanteda)
 library(pacman)
 library(readxl)
 library(quanteda.textstats)
+library(dplyr)
 
+# Set working directory
 setwd("~/Repositories/NLP Charity Research")
+
+# Read Excel spreadsheet file
 mycorpus = read_excel("data/preliminary_march_data/preliminary_data.xlsx")
-# Creating a corpus
+
+# Creating a corpus or dataset/frame
 mission <- corpus(mycorpus$`Mission Statement`, text_field = "text")
-# Tokenisation
+
+# Tokenisation and Remove Stop Words
 tok <- tokens(mission, what = "word",
               remove_punct = TRUE,
               remove_symbols = TRUE,
@@ -17,50 +24,58 @@ tok <- tokens(mission, what = "word",
               verbose = TRUE, 
               include_docvars = TRUE)
 tok <- tokens_tolower(tok)
-
-# Remove Stop Words
 tok <- tokens_select(tok, stopwords("english"), selection = "remove", padding = FALSE)
 
 
-# Readability
+# Readability Section
 # readability <- textstat_readability(mission, c("meanSentenceLength","meanWordSyllables", "Flesch.Kincaid", "Flesch"), remove_hyphens = TRUE,
 #                                     min_sentence_length = 1, max_sentence_length = 10000,
 #                                     intermediate = FALSE)
 
+# Find Flesch-Kincaid readability scores. Lower numbers = easier to read
 mycorpus$readability <- textstat_readability(mission, "Flesch.Kincaid", remove_hyphens = TRUE,
                                              min_sentence_length = 1, max_sentence_length = 10000,
                                              intermediate = FALSE)
+
+# Find Flesch's reading ease scores. Higher numbers = easier to read
 mycorpus$readability2 <- textstat_readability(mission, "Flesch", remove_hyphens = TRUE,
                                              min_sentence_length = 1, max_sentence_length = 10000,
                                              intermediate = FALSE)
+
+# Find mean lengths of sentences (number of words / number of sentences). Note that most mission statements have less than 3 sentences.
 mycorpus$readability3 <- textstat_readability(mission, "meanSentenceLength", remove_hyphens = TRUE,
                                              min_sentence_length = 1, max_sentence_length = 10000,
                                              intermediate = FALSE)
 
-
+# Turn revenue column from spreadsheet to numbers from strings
 as.numeric(mycorpus$Revenue)
 
+# Optional: Remove rows where revenue is 0
 mycorpus = mycorpus[mycorpus$Revenue != 0,]
 
-revenuereadability = lm(mycorpus$Revenue ~ mycorpus$readability$Flesch.Kincaid)
+# Statistical Analysis of Flesch-Kincaid score with revenue
+revenue_FK = lm(mycorpus$Revenue ~ mycorpus$readability$Flesch.Kincaid)
+summary(revenue_FK)
 
-summary(revenuereadability)
+# Statistical Analysis of Flesch score with revenue
+revenue_Flesch = lm(mycorpus$Revenue ~ mycorpus$readability2$Flesch)
+summary(revenue_Flesch)
 
-revenuereadability2 = lm(mycorpus$Revenue ~ mycorpus$readability2$Flesch)
+# Statistical Analysis of mean sentence length with revenue
+revenue_MSL = lm(mycorpus$Revenue ~ mycorpus$readability3$meanSentenceLength)
+summary(revenue_MSL)
 
-summary(revenuereadability2)
+plot(coef(revenue_Flesch)[2])
 
-revenuereadability3 = lm(mycorpus$Revenue ~ mycorpus$readability3$meanSentenceLength)
+# data binning before making scatterplot
+mycorpus %>% mutate(new_bin = cut(mycorpus$readability2$Flesch, breaks=10000000))
 
-summary(revenuereadability3)
-
-plot(coef(revenuereadability2)[2])
-
+# Scatterplot
 plot(x = mycorpus$readability2$Flesch, y = mycorpus$Revenue,
      xlab = "Flesch Readability Score",
      ylab = "Revenue",
      xlim = c(-100, 100),
-     ylim = c(0,10000000),        
+     ylim = c(0,100000000),        
      main = "Revenue vs Flesch Score"
 )
 
